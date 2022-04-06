4c29ea807eff8b2f0c9938912e4addb859d14267
jayamiko <jayamiko4@gmail.com>
2022-04-06 23:26:12 +0700

    slider pagination with swiper

--- src/App.js
+++ src/App.js
@@ -1,64 +1,98 @@
-import { Swiper, SwiperSlide } from "swiper/react";
+import {Swiper, SwiperSlide} from "swiper/react";
 import "swiper/swiper.min.css";
 import "swiper/components/effect-coverflow/effect-coverflow.min.css";
 import "swiper/components/pagination/pagination.min.css";
-import "swiper/components/navigation/navigation.min.css";
 import "./styles.css";
-import SwiperCore, {
-  EffectCoverflow,
-  Pagination,
-  Navigation
-} from "swiper/core";
+import SwiperCore, {EffectCoverflow, Pagination} from "swiper/core";
 
-SwiperCore.use([EffectCoverflow, Pagination, Navigation]);
+SwiperCore.use([EffectCoverflow, Pagination]);
 
 export default function App() {
+  const news = [
+    {
+      title: "LOKAPALA MINOR LEAGUES",
+      category: "Events",
+      updates: "FEB 22",
+      date: "12/15/2021",
+      image: "images/heroes.png",
+    },
+    {
+      title: "LOKAPALA MINOR LEAGUES",
+      category: "Events",
+      updates: "MAR 22",
+      date: "12/15/2021",
+      image: "images/heroes.png",
+    },
+    {
+      title: "LOKAPALA MINOR LEAGUES",
+      category: "Events",
+      updates: "APR 22",
+      date: "12/15/2021",
+      image: "images/heroes.png",
+    },
+    {
+      title: "LOKAPALA MINOR LEAGUES",
+      category: "Events",
+      updates: "MEI 22",
+      date: "12/15/2021",
+      image: "images/heroes.png",
+    },
+    {
+      title: "LOKAPALA MINOR LEAGUES",
+      category: "Events",
+      updates: "JUN 22",
+      date: "12/15/2021",
+      image: "images/heroes.png",
+    },
+    {
+      title: "LOKAPALA MINOR LEAGUES",
+      category: "Events",
+      updates: "AUG 22",
+      date: "12/15/2021",
+      image: "images/heroes.png",
+    },
+    {
+      title: "LOKAPALA MINOR LEAGUES",
+      category: "Events",
+      updates: "SEP 22",
+      date: "12/15/2021",
+      image: "images/heroes.png",
+    },
+  ];
   return (
     <div className="container">
-      <div className="title_wrapper">
-        <div className="reactLogo">
-          <img src="images/react.png" />
-        </div>
-        <div className="title_">
-          <span>React</span>Swiper Slider
-        </div>
-      </div>
+      <div className="title_wrapper"></div>
       <Swiper
-        navigation={true}
-        effect={"coverflow"}
-        centeredSlides={true}
-        slidesPerView={window.innerWidth < 768 ? 1 : "auto"}
-        loop={true}
+        centeredSlides={false}
+        slidesPerView={3}
+        spaceBetween={0}
         coverflowEffect={{
           rotate: 50,
           stretch: 0,
           depth: 100,
           modifier: 1,
-          slideShadows: true
+          slideShadows: true,
         }}
         pagination={{
-          clickable: true
+          clickable: true,
         }}
         className="mySwiper"
       >
-        <SwiperSlide>
-          <img src="images/1.jpg" />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src="images/2.jpg" />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src="images/3.jpg" />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src="images/4.jpg" />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src="images/5.jpg" />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src="images/6.jpg" />
-        </SwiperSlide>
+        {news.map((x, i) => (
+          <SwiperSlide key={i}>
+            <div className="card">
+              <img src={x.image} alt="" />
+              <div className="card_content">
+                <div className="content">
+                  <span className="date">{x.date}</span>
+                  <span className="category">{x.category}</span>
+                  <h4 className="title">{x.title}</h4>
+                  <h4 className="updates">UPDATES {x.updates}</h4>
+                </div>
+              </div>
+            </div>
+          </SwiperSlide>
+        ))}
       </Swiper>
     </div>
   );
